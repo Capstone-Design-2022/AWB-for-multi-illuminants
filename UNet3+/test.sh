5eb9e7e87e647ef738a969dea2b5dcb0faf7b665
JoYunsoo <ysj@yonsei.ac.kr>
2022-06-28 16:47:40 +0900

    codes for UNet3+

---
+++ UNet3+/test.sh
@@ -0,0 +1,2 @@
+#python main.py --mode 'test' --data_root '../galaxy_512' --checkpoint 'pretrained_galaxy/best.pt' --save_result 'yes' --image_pool 1 2 3
+python main.py --mode 'test' --data_root '../galaxy_512' --checkpoint '220504_0307/best_mae_illum.pt' --save_result 'yes' --image_pool 1 2 3
